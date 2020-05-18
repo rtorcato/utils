eae463fb73d17a72dc53d0a0d1ac159c95256fb6
Richard Torcato <rtorcato@me.com>
2020-05-18 16:08:39 -0400

    fix test

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -3,6 +3,6 @@ import { helloWorld } from './../src'
 
 describe('helloWorld', () => {
   it('Should return greetings', () => {
-    expect(helloWorld()).equals('Howdy!')
+    expect(helloWorld()).equals('Howdy!!!')
   })
 })
